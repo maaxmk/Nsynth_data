bf993a3310a4dd506c9f57bcb9cc2b8bc15e0eb2
Max Keene <max@maxs-mbp.attlocal.net>
2018-05-06 17:24:37 -0700

    update

--- nsynth_tools.py
+++ nsynth_tools.py
@@ -142,7 +142,7 @@ def SH(file_full, ipath, opath, SHpos, SHlen, Insert=True, Lead=0):
     newName = file+"_"+str(SHpos)+"_"+str(Lead)+"_"+str(SHlen)
     prefix = name_split[0]+"_"+name_split[1]+"_"
 
-    SHpos = round((len(enc[0])-1)*SHpos)
+    SHpos = int(round((len(enc[0])-1)*SHpos))
 
     A = numpy.zeros((1))
 
